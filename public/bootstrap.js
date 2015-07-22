fbf43ef5db2b97dbe397ffb7ec9b75ad40b956c4
Ed Toro <ed@wyncode.co>
2015-07-22 18:25:24 -0400

    Improve documentation for the livecoding lecture

--- public/bootstrap.js
+++ public/bootstrap.js
@@ -1,16 +1,18 @@
 $(function() {
   // Trigger a modal popup with JavaScript
+  // This can be demo'd in the JS console.
   $('#jsModalButton').click(function() {
     $('#myModal').modal();
   });
 
   // Trigger a dropdown with JavaScript
+  // This can be demo'd in the JS console.
   $('#jsDropdownButton').click(function() {
-    // This code was a lie: $('.dropdown-toggle').dropdown()
+    // This sample code was wrong: $('.dropdown-toggle').dropdown()
     $('.dropdown-menu').last().toggle('dropdown');
   });
 
-  // Tooltips are opt-in because of performance problems
+  // Tooltips are opt-in because... performance
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
